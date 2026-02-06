19420c5f74a4fed933101e8fcee75df8ecda2ced
Ryan Campbell <ryan.campbell4@gmail.com>
2026-02-06 08:16:41 -0600

    convert validator into free functions

--- src/validator/mod.rs
+++ src/validator/mod.rs
@@ -6,130 +6,125 @@ use crate::parser::ast::*;
 use anyhow::{Result, bail};
 use std::collections::HashMap;
 
-struct Validator {
-   variable_map: HashMap<String, String>
+type VariableMap = HashMap<String, (String, bool)>;
+
+fn copy_variable_map(map: &HashMap<String, (String, bool)>) -> HashMap<String, (String, bool)> {
+   let mut new_map = HashMap::new();
+   for (key, value) in map {
+      new_map.insert(key.clone(), (value.0.clone(), false));
+   }
+   new_map
 }
 
 pub fn validate(ast: &mut AST, print_ast: bool) -> Result<()> {
-   let mut validator = Validator::new();
-   validator.validate(ast)?;
+   let mut variable_map: VariableMap = HashMap::new();
+   resolve_program(&mut ast.program, &mut variable_map)?;
    if print_ast {
       ast_printer::print_ast(&ast);
    }
    Ok(())
 }
 
-impl Validator {
-   fn new() -> Self {
-      Self {
-         variable_map: HashMap::new()
+fn resolve_program(program: &mut Program, variable_map: &mut VariableMap) -> Result<()> {
+   match program {
+      Program::FunctionDefinition(FunctionDefinition{ name: _, body }) => {
+         resolve_function(body, variable_map)?;
+         Ok(())
       }
    }
+}
 
-   fn validate(&mut self, ast: &mut AST) -> Result<()> {
-      self.resolve_program(&mut ast.program)?;
-      Ok(())
+fn resolve_function(body: &mut Block, variable_map: &mut VariableMap) -> Result<()> {
+   resolve_block(body, variable_map)
+}
+
+fn resolve_block(block: &mut Block, variable_map: &mut VariableMap) -> Result<()> {
+   for block_item in &mut *block.items {
+      resolve_block_item(block_item, variable_map)?;
    }
+   Ok(())
+}
 
-   fn resolve_program(&mut self, program: &mut Program) -> Result<()> {
-      match program {
-         Program::FunctionDefinition(FunctionDefinition{ name: _, body }) => {
-            self.resolve_function(body)?;
-            Ok(())
+fn resolve_block_item(item: &mut BlockItem, variable_map: &mut VariableMap) -> Result<()> {
+   match item {
+      BlockItem::Stmt(stmt) => {
+         resolve_statement(stmt, variable_map)?;
+      },
+      BlockItem::Decl(decl) => {
+         resolve_declaration(decl, variable_map)?;
+      }
+   }
+   Ok(())
+}
+
+fn resolve_statement(stmt: &mut Stmt, variable_map: &mut VariableMap) -> Result<()> {
+   match stmt {
+      Stmt::Expression(e) => {
+         resolve_expr(e, variable_map)?;
+      },
+      Stmt::Return(e) => {
+         resolve_expr(e, variable_map)?;
+      },
+      Stmt::Null => (),
+      Stmt::If(expr, then_stmt, else_stmt) => {
+         if let Some(else_stmt) = else_stmt {
+            resolve_statement(else_stmt, variable_map)?;
          }
+         resolve_expr(expr, variable_map)?;
+         resolve_statement(then_stmt, variable_map)?;
+      },
+      Stmt::Compound(block) => {
+         resolve_block(block, variable_map)?;
       }
    }
+   Ok(())
+}
 
-   fn resolve_function(&mut self, body: &mut Block) -> Result<()> {
-      for block_item in &mut *body.items {
-         self.validate_block_item(block_item)?;
-      }
-      Ok(())
+fn resolve_declaration(decl: &mut Decl, variable_map: &mut VariableMap) -> Result<()> {
+   let Decl::Decl(name, initializer, line_number) = decl;
+   if variable_map.contains_key(name) {
+      bail!(error::error(*line_number, format!("\"{}\" already declared.", name), error::ErrorType::SemanticError))
    }
+   let unique_name = name_generator::uniquify_identifier(name);
+   variable_map.insert(name.clone(), (unique_name.clone(), true));
 
-   fn validate_block_item(&mut self, item: &mut BlockItem) -> Result<()> {
-      match item {
-         BlockItem::Stmt(stmt) => {
-            self.resolve_statement(stmt)?;
-         },
-         BlockItem::Decl(decl) => {
-            self.resolve_declaration(decl)?;
+   if let Some(expr) = initializer {
+      resolve_expr(expr, variable_map)?;
+   }
+   *name = unique_name;
+   Ok(())
+}
+
+fn resolve_expr(expr: &mut Expr, variable_map: &mut VariableMap) -> Result<()> {
+   match expr {
+      Expr::Assignment(left, right, line_number) => {
+         if let Expr::Var(_, _) = **left {
+            resolve_expr(left, variable_map)?;
+            resolve_expr(right, variable_map)?;
+         } else {
+            bail!(error::error(*line_number, format!("Invalid lvalue"), error::ErrorType::SemanticError))
          }
-      }
-      Ok(())
-   }
-
-   fn resolve_statement(&mut self, stmt: &mut Stmt) -> Result<()> {
-      match stmt {
-         Stmt::Expression(e) => {
-            self.resolve_expr(e)?;
-         },
-         Stmt::Return(e) => {
-            self.resolve_expr(e)?;
-         },
-         Stmt::Null => (),
-         Stmt::If(expr, then_stmt, else_stmt) => {
-            if let Some(else_stmt) = else_stmt {
-               self.resolve_statement(else_stmt)?;
-            }
-            self.resolve_expr(expr)?;
-            self.resolve_statement(then_stmt)?;
-         },
-         Stmt::Compound(block) => {
-            for block_item in &mut *block.items {
-               self.validate_block_item(block_item)?;
-            }
+      },
+      Expr::Var(name, line_number) => {
+         if let Some(_entry @ (unique_name, _)) = variable_map.get(name) {
+            *name = unique_name.clone();
+         } else {
+            bail!(error::error(*line_number, format!("Undeclared variable {}", name), error::ErrorType::SemanticError))
          }
+      },
+      Expr::BinaryOp(_, left, right) => {
+         resolve_expr(left, variable_map)?;
+         resolve_expr(right, variable_map)?;
+      },
+      Expr::Integer(_) => (),
+      Expr::UnaryOp(_, expr) => {
+         resolve_expr(expr, variable_map)?;
+      },
+      Expr::Conditional(condition, middle, right) => {
+         resolve_expr(condition, variable_map)?;
+         resolve_expr(middle, variable_map)?;
+         resolve_expr(right, variable_map)?;
       }
-      Ok(())
    }
-
-   fn resolve_declaration(&mut self, decl: &mut Decl) -> Result<()> {
-      let Decl::Decl(name, initializer, line_number) = decl;
-      if self.variable_map.contains_key(name) {
-         bail!(error::error(*line_number, format!("\"{}\" already declared.", name), error::ErrorType::SemanticError))
-      }
-      let unique_name = name_generator::uniquify_identifier(name);
-      self.variable_map.insert(name.clone(), unique_name.clone());
-
-      if let Some(expr) = initializer {
-         self.resolve_expr(expr)?;
-      }
-      *name = unique_name;
-      Ok(())
-   }
-
-   fn resolve_expr(&mut self, expr: &mut Expr) -> Result<()> {
-      match expr {
-         Expr::Assignment(left, right, line_number) => {
-            if let Expr::Var(_, _) = **left {
-               self.resolve_expr(left)?;
-               self.resolve_expr(right)?;
-            } else {
-               bail!(error::error(*line_number, format!("Invalid lvalue"), error::ErrorType::SemanticError))
-            }
-         },
-         Expr::Var(name, line_number) => {
-            if let Some(unique_name) = self.variable_map.get(name) {
-               *name = unique_name.clone();
-            } else {
-               bail!(error::error(*line_number, format!("Undeclared variable {}", name), error::ErrorType::SemanticError))
-            }
-         },
-         Expr::BinaryOp(_, left, right) => {
-            self.resolve_expr(left)?;
-            self.resolve_expr(right)?;
-         },
-         Expr::Integer(_) => (),
-         Expr::UnaryOp(_, expr) => {
-            self.resolve_expr(expr)?;
-         },
-         Expr::Conditional(condition, middle, right) => {
-            self.resolve_expr(condition)?;
-            self.resolve_expr(middle)?;
-            self.resolve_expr(right)?;
-         }
-      }
-      Ok(())
-   }
+   Ok(())
 }
